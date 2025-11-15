0a8268872aaadaeacf7c10d66d47738386c27681
Jeisson Sanchez <jeisson.sanchez-g@mail.escuelaing.edu.co>
2025-11-14 22:08:19 -0500

    Config Inicial + Terraform apply

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" { type = string }
+variable "location" { type = string }
+variable "vm_count" { type = number }
+variable "admin_username" { type = string }
+variable "ssh_public_key" { type = string }
+variable "allow_ssh_from_cidr" { type = string }
+variable "tags" { type = map(string) }
+variable "subscription_id" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
+
